40598bf9b5d7b9e8f67881e0a2ea0c8c4e624c0f
hhuds <hhuds@LAPTOP-BAELTCGM>
2023-04-21 17:59:59 -0400

    Add project files.

---
+++ P7hh/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace P7hh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.slotMachineImageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.slotMachineImageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.slotMachineImageList3 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slotMachineImageList1
+            // 
+            this.slotMachineImageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("slotMachineImageList1.ImageStream")));
+            this.slotMachineImageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.slotMachineImageList1.Images.SetKeyName(0, "Apple.bmp");
+            this.slotMachineImageList1.Images.SetKeyName(1, "Banana.bmp");
+            this.slotMachineImageList1.Images.SetKeyName(2, "Cherries.bmp");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 133);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 133);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(293, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 133);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // spinButton
+            // 
+            this.spinButton.Location = new System.Drawing.Point(136, 208);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(75, 23);
+            this.spinButton.TabIndex = 3;
+            this.spinButton.Text = "Spin";
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(222, 208);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Amount Inserted $";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(222, 172);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.amountTextBox.TabIndex = 6;
+            // 
+            // slotMachineImageList2
+            // 
+            this.slotMachineImageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("slotMachineImageList2.ImageStream")));
+            this.slotMachineImageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.slotMachineImageList2.Images.SetKeyName(0, "Apple.bmp");
+            this.slotMachineImageList2.Images.SetKeyName(1, "Banana.bmp");
+            this.slotMachineImageList2.Images.SetKeyName(2, "Cherries.bmp");
+            // 
+            // slotMachineImageList3
+            // 
+            this.slotMachineImageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("slotMachineImageList3.ImageStream")));
+            this.slotMachineImageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.slotMachineImageList3.Images.SetKeyName(0, "Apple.bmp");
+            this.slotMachineImageList3.Images.SetKeyName(1, "Banana.bmp");
+            this.slotMachineImageList3.Images.SetKeyName(2, "Cherries.bmp");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 250);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList slotMachineImageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.ImageList slotMachineImageList2;
+        private System.Windows.Forms.ImageList slotMachineImageList3;
+    }
+}
+
---
+++ P7hh/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace P7hh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare variables
+                decimal amount;         // Amount of money the player wants to gamble
+
+                // Get the amount from the TextBox
+                amount = decimal.Parse(amountTextBox.Text);
+
+                // Create a Random object
+                Random rand = new Random();
+
+                // Get a random indexes
+                int index1 = rand.Next(slotMachineImageList1.Images.Count);
+                int index2 = rand.Next(slotMachineImageList2.Images.Count);
+                int index3 = rand.Next(slotMachineImageList3.Images.Count);
+
+                //Assign random objects/images to pictureBoxes
+                pictureBox1.Image = slotMachineImageList1.Images[index1];
+                pictureBox2.Image = slotMachineImageList2.Images[index2];
+                pictureBox3.Image = slotMachineImageList3.Images[index3];
+
+                // Compare the random objects
+                if (index1 == index2 && index1 == index3 || index2 == index3 && index2 == index1)
+                {
+                    amount = amount * 3;
+                    MessageBox.Show("JACKPOT!!! You have won $" + amount);
+                }
+                else
+                {
+                    if (index1 != index2 && index1 == index3 || index1 == index2 && index1 != index3 || index2 == index3 && index2 != index1)
+                    {
+                        amount = amount * 2;
+                        MessageBox.Show("Congratulations! You have won $" + amount);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry. You Lose.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //CLose the form
+            this.Close();
+        }
+    }
+}
